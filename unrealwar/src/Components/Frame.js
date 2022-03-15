7fe4de1e4996b75eed868b625efd1f29eb11db4b
JackFrathbone <2282711@students.wits.ac.za>
2022-03-15 22:32:22 +0200

    Some framing

---
+++ unrealwar/src/Components/Frame.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Frame extends Component {
+  render() {
+    return <div>Frame</div>;
+  }
+}
---
+++ unrealwar/src/Components/VideoPlayer.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class VideoPlayer extends Component {
+  render() {
+    return <div>VideoPlayer</div>;
+  }
+}
